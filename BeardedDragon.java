2b3a85ffb81b9926cde9b5421f754ce201188409
gonzalodevarona <gavo2403@hotmail.com>
2019-02-27 13:08:22 -0500

    Update BeardedDragon.java

--- BeardedDragon.java
+++ BeardedDragon.java
@@ -5,7 +5,7 @@
  * DEPARTAMENTO TIC - ALGORTIMOS Y PROGRAMACIÓN I
  * LAB FOR CALI ZOO CODE
  * @AUTHOR: GONZALO DE VARONA <gonzalo.de1@correo.icesi.edu.co>
- * @DATE: 23 FEBRUARY 2019
+ * @LAST UPDATE DATE: 23 FEBRUARY 2019
  * ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
  */
 
@@ -108,7 +108,7 @@ public class BeardedDragon
 		this.birthDateD = birthDateD;
 	}
 
-	public double calculateBmi () {
+	public double calculateBmi() {
 
 		bmi = getWeight() / (getHeight() * getHeight());
 		return bmi;
