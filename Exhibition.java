c2c54f5616f08dfb253884a8e00c3ea99c53ab37
gonzalodevarona <gavo2403@hotmail.com>
2019-03-03 14:18:24 -0500

    Update Exhibition.java

--- Exhibition.java
+++ Exhibition.java
@@ -5,7 +5,7 @@
  * DEPARTAMENTO TIC - ALGORTIMOS Y PROGRAMACIÓN I
  * LAB FOR CALI ZOO CODE
  * @AUTHOR: GONZALO DE VARONA <gonzalo.de1@correo.icesi.edu.co>
- * @LAST UPDATE DATE: 1 MARCH 2019
+ * @LAST UPDATE DATE: 3 MARCH 2019
  * ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
  */
 
@@ -76,9 +76,7 @@ public class Exhibition
 	public void kangarooList(){
 
 
-						System.out.println("");
-						System.out.println("Please select which kangaroo will be erased:");
-						System.out.println("");
+						
 						if(zooKangaroos.getEnvironmentA().getKangarooA() != null){
 						System.out.println("1. " +zooKangaroos.getEnvironmentA().getKangarooA().getName());	
 						} 
@@ -127,5 +125,151 @@ public class Exhibition
 		return theList;
 	}
 
+
 	
-}
+
+	public void executeCreate(Kangaroo theNew, int environment){
+	zooKangaroos.createKangaroo(theNew, environment);
+	}
+
+
+		public String convertBloodType(int userInput){
+		String bloodType = "";
+
+			switch (userInput) {
+				case 1: bloodType += "O";
+				break;
+				case 2: bloodType += "A";
+				break;
+				case 3: bloodType += "AB";
+				break;
+				case 4: bloodType += "B";
+				break;
+				default: bloodType += "ERROR";
+				break;
+				
+			}
+			
+
+			return bloodType;
+	}
+
+
+	
+	
+
+
+
+	/*public String placeKangarooGender(){
+		String message = "";
+		switch (zooKangaroos.checkSelectedEnvironmentGender(environment)) {
+			case true: message = "ERROR: The kangaroo can not be place in this environment because there is already one male.";
+			break;
+			case false:
+			break;
+		 } 
+		 return message;
+	} */
+
+
+	public void kangarooListWE(){
+						System.out.println("");
+						System.out.println("Those are the kangaroos from the first environment: ");	
+						
+						if(zooKangaroos.getEnvironmentA().getKangarooA() != null){
+						System.out.println("1. " +zooKangaroos.getEnvironmentA().getKangarooA().getName());	
+						} 
+						
+						if(zooKangaroos.getEnvironmentA().getKangarooB() != null){
+						System.out.println("2. "+zooKangaroos.getEnvironmentA().getKangarooB().getName());	
+						} 
+						
+						if(zooKangaroos.getEnvironmentA().getKangarooC() != null){
+						System.out.println("3. "+zooKangaroos.getEnvironmentA().getKangarooC().getName());	
+						}
+
+						System.out.println("");
+						System.out.println("");
+
+
+						System.out.println("Those are the kangaroos from the second environment: ");	
+						
+						if(zooKangaroos.getEnvironmentB().getKangarooA() != null){
+						System.out.println("4. "+zooKangaroos.getEnvironmentB().getKangarooA().getName());	
+						}
+						
+						if(zooKangaroos.getEnvironmentB().getKangarooB() != null){
+						System.out.println("5. "+zooKangaroos.getEnvironmentB().getKangarooB().getName());	
+						}
+						
+						if(zooKangaroos.getEnvironmentB().getKangarooC() != null){
+						System.out.println("6. "+zooKangaroos.getEnvironmentB().getKangarooC().getName());	
+						}
+
+						System.out.println("");
+						System.out.println("");
+
+
+						System.out.println("Those are the kangaroos from the third environment: ");	
+						
+						if(zooKangaroos.getEnvironmentC().getKangarooA() != null){
+						System.out.println("7. "+zooKangaroos.getEnvironmentC().getKangarooA().getName());	
+						}
+						
+						if(zooKangaroos.getEnvironmentC().getKangarooB() != null){
+						System.out.println("8. "+zooKangaroos.getEnvironmentC().getKangarooB().getName());	
+						}
+						
+						if(zooKangaroos.getEnvironmentC().getKangarooC() != null){
+						System.out.println("9. "+zooKangaroos.getEnvironmentC().getKangarooC().getName());	
+						}
+						
+						System.out.println("");
+					
+						System.out.println("0. BACK");
+
+
+						System.out.println("");
+
+
+	}
+
+	public void environmentPosibilities4A(){
+	System.out.println("");
+	System.out.println("Where do you wanna change it to?");
+	System.out.println("1. Second environment");
+	System.out.println("2. Third environment");
+	System.out.println("");
+
+
+	}
+
+
+	public void environmentPosibilities4B(){
+	System.out.println("");
+	System.out.println("Where do you wanna change it to?");
+	System.out.println("1. First environment");
+	System.out.println("2. Third environment");
+	System.out.println("");
+
+
+	}
+
+
+	public void environmentPosibilities4C(){
+	System.out.println("");
+	System.out.println("Where do you wanna change it to?");
+	System.out.println("1. First environment");
+	System.out.println("2. Second environment");
+	System.out.println("");
+
+
+	}
+
+
+
+
+
+
+	
+}//final
